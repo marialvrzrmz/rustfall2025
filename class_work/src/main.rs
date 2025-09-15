ee32ef225ad7a2ebe3f2db579da8ca05aab1fcd4
Maria F Alvarez Ramirez <115670543+marialvrzrmz@users.noreply.github.com>
2025-09-15 15:58:59 +0000

    rust pattern and basics

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,21 +1,18 @@
-fn main() {
-    let x = 10;
-    //let x = x + 1;
-    
-        let x = x + 10;
+fn pattern_match_simple() {
+    let by3 = num%3 == 0;
+    let by5 = num%5 == 0;
 
-        println!("{}", x);
-    
-    
-        let x = x + 10;
+    let result == match(by3, by5){
+        (true, true) => "FizzBuzz".to_string(),
+        (true, false) => "Fizz".to_string(),
+        (false, true) => "Buzz".to_string(),
+        (false, false) => num.to_string(),
+    };
+    result
+}
 
-        println!("{}", x);
-    
-    
-        let x = x + 10;
-
-        println!("{}", x);
-    
-
-    println!("{}", x);
+fn main(){
+    for num in 1...20{
+        println!("{}", pattern_match_simple(num));
+    }
 }
