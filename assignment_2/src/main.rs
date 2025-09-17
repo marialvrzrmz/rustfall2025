7d60a86a00f2c607efcfb326d0e0f771f8539c5b
Maria F Alvarez Ramirez <115670543+marialvrzrmz@users.noreply.github.com>
2025-09-17 15:40:00 +0000

    just working on assign 2

--- assignment_2/src/main.rs
+++ assignment_2/src/main.rs
@@ -1,3 +1,10 @@
+fn is_even(n: i32) -> bool{
+    let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for nums in nums.iter(){
+        
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
